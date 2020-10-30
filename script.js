ef68f62f07669acffd243c9aeaa126f7bedcfffa
Sinyagin18V2 <62714375+77Rbk77@users.noreply.github.com>
2020-10-30 23:04:10 +0300

    Added 1 lab

---
+++ script.js
@@ -0,0 +1,9 @@
+﻿ document.querySelector('.block').onmousemove = function(event) { 
+    document.querySelector('.x').innerHTML = event.offsetX; 
+    document.querySelector('.y').innerHTML = event.offsetY; 
+}
+
+document.onkeypress = function(event){ 
+	document.getElementById('key').innerHTML = event.keyCode; 
+	document.getElementById("key").style.display = "block"; 
+}
